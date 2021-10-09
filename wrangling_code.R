340e8183c4f74ad5e922892071e40d809c95bcd4
Mi Zhang <mizh@bu.edu>
2021-10-09 02:10:48 -0400

    add comments

--- wrangling_code.R
+++ wrangling_code.R
@@ -2,19 +2,28 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
+#started with reading both data sets that I downloaded from gapminder.org
+
 male_employment <- 
   read.csv("~/desktop/MA_615/ma615_assignment2/males_aged_15plus_employment_rate_percent.csv", header=TRUE, check.names = FALSE)
 female_employment<- 
   read.csv("~/desktop/MA_615/ma615_assignment2/females_aged_15plus_employment_rate_percent.csv", 
            header=TRUE, check.names = FALSE)
+
+#using tidyr command to separate two variables with given names
+#using the pipe to help me keep track of my data 
+#pivot_long also transform data from row to columns which is more readable
 m_empl <- male_employment %>%
   pivot_longer(!country, names_to = "year", values_to = "male")
 f_empl <- female_employment %>%
   pivot_longer(!country, names_to = "year", values_to = "female") 
 
-# combine male and female employment rate in one tibble 
+# combine male and female employment rate in one tibble using inner_join()
+#matching the row by the key(year)
 employ<- m_empl %>% inner_join(f_empl)
 
+#using dplyr:filter() to help me choose my interested countries 
+#and aiming for comparison
 employment <- employ %>% 
   filter(country == "China"|country=="Canada")
  
